9412f3a2f3c4a6ee7ad0e3e7df997fedaae30070
Michal Witek <michalwitek1232@gmail.com>
2022-06-06 19:06:34 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTableModule,
-    NgModule
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
